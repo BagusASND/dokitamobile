2a06af6b71393130d87b7edb38e8e07ec028502e
bagusasnd <barisandi21@gmail.com>
2018-07-25 14:00:52 +0700

    update home page

--- assets/js/custom.js
+++ assets/js/custom.js
@@ -4,3 +4,24 @@ $(document).ready(function(){
         autoplaySpeed: 2000
     });
 });
+
+
+
+$('#zoomBtn').click(function() {
+    $('.zoom-btn-sm').toggleClass('scale-out');
+    if (!$('.zoom-card').hasClass('scale-out')) {
+        $('.zoom-card').toggleClass('scale-out');
+        /*$('#widgetWrap').addClass('overlay-widget');*/
+        $('#phoneBtn').addClass('hide-temp');
+        $('#phoneCloseBtn').removeClass('hide-temp');
+    }
+});
+
+$('.zoom-btn-sm').click(function() {
+    var btn = $(this);
+    var card = $('.zoom-card');
+
+    if ($('.zoom-card').hasClass('scale-out')) {
+        $('.zoom-card').toggleClass('scale-out');
+    }
+});
