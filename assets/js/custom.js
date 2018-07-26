d1174f7cf92dc833efec2f7ce007d06d41b303fd
bagusasnd <barisandi21@gmail.com>
2018-07-26 19:11:39 +0700

    update home 26/7/18

--- assets/js/custom.js
+++ assets/js/custom.js
@@ -3,25 +3,31 @@ $(document).ready(function(){
         autoplay: true,
         autoplaySpeed: 2000
     });
-});
 
+
+});
 
 
 $('#zoomBtn').click(function() {
     $('.zoom-btn-sm').toggleClass('scale-out');
+
     if (!$('.zoom-card').hasClass('scale-out')) {
         $('.zoom-card').toggleClass('scale-out');
-        /*$('#widgetWrap').addClass('overlay-widget');*/
-        $('#phoneBtn').addClass('hide-temp');
-        $('#phoneCloseBtn').removeClass('hide-temp');
+
+        if($('.zoom-btn-sm').hasClass('scale-out')){
+            $('#widgetWrap').removeClass('overlay-widget');
+            $('#phoneBtn').addClass('fa-phone');
+            $('#phoneBtn').removeClass('fa-times');
+        }
+        else{
+            $('#widgetWrap').addClass('overlay-widget');
+            $('#phoneBtn').removeClass('fa-phone');
+            $('#phoneBtn').addClass('fa-times');
+        }
     }
 });
 
-$('.zoom-btn-sm').click(function() {
-    var btn = $(this);
-    var card = $('.zoom-card');
 
-    if ($('.zoom-card').hasClass('scale-out')) {
-        $('.zoom-card').toggleClass('scale-out');
-    }
+$('#dropdownBtn').click(function() {
+    $('.dropdown-content.info').toggle();
 });
