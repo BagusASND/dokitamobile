70a3e21277e3d5d5ef5ea12991e5d12b92fb9d09
bagusasnd <barisandi21@gmail.com>
2018-07-31 19:29:40 +0700

    create slide effect

--- assets/js/custom.js
+++ assets/js/custom.js
@@ -3,8 +3,45 @@ $(document).ready(function(){
         autoplay: true,
         autoplaySpeed: 2000
     });
+
+    $('#showMore').click(function () {
+        $('.content-more').removeClass('hide-temp').effects( scale, options, 500, callback );
+    })
 });
 
+$( function() {
+    // run the currently selected effect
+    function runEffect() {
+        // get effect type from
+        var selectedEffect = "slide";
+
+        // Most effect types need no options passed by default
+        var options = {};
+        // some effects have required parameters
+        if ( selectedEffect === "scale" ) {
+            options = { percent: 50 };
+        } else if ( selectedEffect === "size" ) {
+            options = { to: { width: 280, height: 185 } };
+        }
+
+        // Run the effect
+        $( "#effect" ).show( selectedEffect, options, 500, callback );
+    };
+
+    //callback function to bring a hidden box back
+    function callback() {
+        /*setTimeout(function() {
+            $( "#effect:visible" ).removeAttr( "style" ).fadeOut();
+        }, 1000 );*/
+    };
+
+    // Set effect from select menu value
+    $( "#button" ).on( "click", function() {
+        runEffect();
+    });
+
+    $( "#effect" ).hide();
+} );
 
 $('#zoomBtn').click(function() {
     $('.zoom-btn-sm').toggleClass('scale-out');
